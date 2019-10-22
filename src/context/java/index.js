81d28aa7dbfd8bc83d8bd60f8bac8e82d7b462b6
akter <akter.hossain@nhst.com>
2019-10-22 15:07:38 +0600

    update data

--- src/context/java/index.js
+++ src/context/java/index.js
@@ -1,17 +1,17 @@
 export * from './masthead';
 
 export const about = `
-I am a full-stack developer working for 6+ years in Java and JavaScript stack.
- Other than that, I am an open stack enthusiast. Reading blog, learning new stacks
-  exploring cities are in the list of my top favorite hobbies.
+Full-stack developer working for 6+ years in Java and JavaScript stack.
+ Open stack enthusiast. In leisure periods, read tech blog, learn trending
+  tech & tools. Exploring cities and countryside are in the top of the list of hobbies.
 `;
 
 export const workflow = `
 Beginning of a feature implementation or solving a problem, I frame box the 
 problem/feature within some design patterns to keep the implementation clean 
-and scalable. While writing code, I focus on design principles as well to 
-keep it readable and maintainable. As a passionate software developer, I write 
-tests of my code, do daily scrum, review others code, share what I learn newly 
+and scalable. While writing code, I focus on design principles to 
+keep code readable and maintainable. As a passionate software developer, I write 
+tests of my code, do daily scrum, review pull request, share own opinion 
 and thus try to play the role of a team player.
 `;
 
@@ -20,7 +20,7 @@ export const skillSet = [
     label: 'Backend',
     skills: [
       'Java 8, Kotlin, Groovy, Node.js',
-      'Spring Boot, Webflux, Cloud, ExpressJS, TypeORM',
+      'Spring Boot, Webflux, Hibernate, ExpressJS, TypeORM',
       'PostgreSQL, MySQL, MongoDB, ElasticSearch',
       'JUnit 5, Mockito',
     ]
@@ -49,20 +49,20 @@ export const skillSet = [
 
 export const projects = [
   {
-    title: 'Content Publication System',
-    description: `A SaaS model (multi-tenant) web publication system. Serving custom
-    data structure, themes and plugable custom features from same instance was 
-    the most challenging part. The backend system architecture includes multiple 
+    title: 'News Publication System',
+    description: `A SaaS (multi-tenant) web publication system. The most challengin
+    part is to serve custom data structures, themes and features from same instance 
+    of the server. The backend system architecture includes multiple 
     <b>microservices</b> using <b>Java 8, Spring, RabbitMQ, ElasticSearch, S3, Docker,
     Kubernetes</b> etc. The frontends are built on top of <b>Node.js, React & Vue</b>.`
   },
   {
     title: 'E-commerce CMS',
-    description: `A full-stack e-commerce CMS. Supports third party plugin system.
-    Besides contributing in core CMS development I wrote few plugins which are 
-    related to product recommendation, minimum shipping box size calculator,
-    user interests etc. <b>Java, JavaScript, Spring, Hibernate, Grails</b> are mostly
-    used tech stack.`,
+    description: `A full-stack e-commerce CMS. The mentionable features are curated
+    user feed according to their interest and buying history, caching
+    product details page, real time customer review & third party plugin like
+    related product, recommended product, minimum shipping box size calculator etc.
+    `
   },
 ];
 
@@ -80,34 +80,34 @@ export const educations = [
 export const experiences = [
   {
     title: 'Software Engineer',
-    duration: ['Dec 2017', 'Ongoing'],
+    duration: ['December 2017', 'Ongoing'],
     company: {
       title: 'Cefalo Bangladesh Ltd',
       website: 'http://www.cefalo.com',
     },
     roles: [
-      `Work as a consultant for <a class="inline-link" target="_blank" href="https://www.nhst.no/">NHST Media Group, Norway</a>`,
+      `Working as a consultant for <a class="inline-link" target="_blank" href="https://www.nhst.no/">NHST Media Group, Norway</a>`,
       'Lead and maintain a full-stack project based on Node.js & Vue',
       'Write & review code of several Java based Microservices',
-      'Follow Agile, SCRUM, TDD, CI/CD using Jenkins on OpenShift'
+      'Follow Agile, SCRUM, TDD, CI/CD (Jenkins on OpenShift)'
     ]
   }, {
-    title: 'Software Developer',
-    duration: ['Sept 2014', 'Dec 2017'],
+    title: 'Software Engineer',
+    duration: ['September 2014', 'December 2017'],
     company: {
       title: 'Dynamic Solution Innovators Ltd',
       website: 'http://www.dsinnovators.com',
     },
     roles: [
       `Worked as a software consultant for <a class="inline-link" target="_blank" href="https://www.jenzabar.com/">Jenzabar, USA</a>`,
-      'Managed a small full-stack development team with 5 developers and QA',
-      'Worked directly to the product owner to collect new features and requirements',
-      'Maintain daily stand up meeting, test progress and production deployments',
+      'Managed a full-stack development team consisting 5 developers and QA',
+      'Worked directly to the product owner to collect new feature requirements',
+      'Maintained daily stand up meeting, test progress and production deployments',
       'Developed a Java functional library using lambda on top of legacy custom Java EE framework'
     ]
   }, {
-    title: 'Junior Software Developer',
-    duration: ['Aug 2013', 'Sept 2014'],
+    title: 'Software Developer',
+    duration: ['August 2013', 'September 2014'],
     company: {
       title: 'Bit Mascot (Pvt.) Ltd',
       website: 'https://www.bitmascot.com',
@@ -115,7 +115,7 @@ export const experiences = [
     roles: [
       'Worked as a full-stack developer in an e-commerce CMS project',
       'Used Java, Spring, Hibernate and JavaScript',
-      'Regularly attended tech sessions'
+      'Got experience in production grade support'
     ]
   },
 ];
