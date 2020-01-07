f478f9a84623798f8fb2709f4fa1139f08262b50
akter <akter.hossain@nhst.com>
2020-01-07 19:05:19 +0600

    update java context

--- src/context/java/projects.js
+++ src/context/java/projects.js
@@ -1,10 +1,11 @@
 export const projects = [
   {
     title: 'News Publication System',
-    description: `A multi-tenant SaaS eco-system. It has almost 10+ 
-    <b>microservices</b> using <b>Java 8, Spring, RabbitMQ, ElasticSearch, S3, Docker,
-    Kubernetes</b> etc. The main challenging part is to support multiple clients(so far 12)
-    from the single instance of service (SaaS).`
+    description: `A multi-tenant SaaS eco-system. It contains 10+ 
+    <b>microservices</b> and most of them are based on 
+    <b>Java 8, Spring, RabbitMQ, ElasticSearch, S3, Docker,
+    Kubernetes</b> etc. The main challenging part is to support
+    multiple clients(so far 12) from the single instance of service (SaaS).`
   },
   {
     title: 'E-commerce CMS',
