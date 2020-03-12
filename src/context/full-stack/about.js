7c34b8df0a682b28d16086202e694c4de66600fe
akter <akter.hossain@nhst.com>
2020-03-12 19:14:21 +0600

    update about

--- src/context/full-stack/about.js
+++ src/context/full-stack/about.js
@@ -2,5 +2,5 @@ export const about = `
 Full-stack developer working for 6+ years in Java and JavaScript stack.
  Write readable, testable & maintainable code.
  Open stack enthusiast. Follow trending tech & tools.
- Explore cities and countrysides very often and in vacation.
+ Explore cities and countrysides.
 `;
